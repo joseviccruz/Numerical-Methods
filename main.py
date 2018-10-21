fdba2be92693069d1939e4696b536dbeda1a9b98
José Victor Cruz <jvsc@cin.ufpe.br>
2018-10-21 12:51:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from sympy import *
+# [!!!] import matplotlib.pyplot as plt
 import sys
 
 maxn = int(1e3 + 10)
 
 coef_ab = maxn * [-1]
 coef_am = maxn * [-1]
-coef_inv = [
-    [1],
-    [1, 1],
-    [2/3, 4/3, -1/3],
-    [6/11, 18/11, -9/11, 2/11],
-    [12/25, 48/25, -36/25, 16/25, -3/25],
-    [60/137, 300/137, -300/137, 200/137, -75/137, 12/137],
-    [60/147, 360/147, -450/147, 400/147, -255/147, 72/147, -10/147]        
-]
+coef_inv = maxn * [-1]
 
 def freopen(filename: str, mode: str):
     if mode == 'r':
@@ -53,6 +46,27 @@ def beta_am(order: int, i: int):
             coef_am[s].append(integrate(f(t), (t, 0, 1)) * ((-1) ** (s - j)) / (factorial(j) * factorial(s - j)))
     return coef_am[order][i]
 
+def beta_inv(order: int, i: int):
+    if(coef_inv[order] == -1):
+        s = order
+        t = symbols('t')
+        coef_inv[order] = []
+        for j in range(0, s + 1):
+            args = '1'
+            for k in range(0, s + 1):
+                if k == j:
+                    continue
+                args += ' * (t - ' + str(k) + ') / (' + str(j) + ' - ' + str(k) + ')' 
+            f = sympify(args)
+            f = diff(f, t)
+            f = lambdify((t), f, 'numpy')
+            ans = f(0)
+            if j == 0:
+                coef_inv[order].append(-1 / ans)
+            else:
+                coef_inv[order].append(coef_inv[order][0] * ans)
+    return coef_inv[order][i]
+
 def evalf(expr: str):
     expr = sympify(expr)
     t, y = symbols('t y')
@@ -87,11 +101,11 @@ def runge_kutta(y: list, t0: float, h: float, steps: int, f):
     ans = []
     for i in range(0, steps + 1):
         ans.append(y[0])
-        k[1] = h * f(t0, y[0])
-        k[2] = h * f(t0 + (h / 2), y[0] + (k[1] / 2))
-        k[3] = h * f(t0 + (h / 2), y[0] + (k[2] / 2))
-        k[4] = h * f(t0 + h, y[0] + k[3])
-        y[0] += (1 / 6) * (k[1] + 2 * k[2] + 2 * k[3] + k[4])
+        k[1] = f(t0, y[0])
+        k[2] = f(t0 + (h / 2), y[0] + k[1] * (h / 2))
+        k[3] = f(t0 + (h / 2), y[0] + k[2] * (h / 2))
+        k[4] = f(t0 + h, y[0] + k[3] * h)
+        y[0] += (h / 6) * (k[1] + 2 * k[2] + 2 * k[3] + k[4])
         t0 += h
     return ans
 
@@ -110,9 +124,9 @@ def get_next_am(y: list, t0: float, h: float, steps: int, f, order: int):
 
 def get_next_inv(y: list, t0: float, h: float, steps: int, f, order: int):
     sz = order + 1
-    y_n = coef_inv[order][0] * h * f(t0 + order * h, y[-1])
+    y_n = beta_inv(order, 0) * h * f(t0 + order * h, y[-1])
     for j in range(-sz, -1):
-        y_n += coef_inv[order][-(sz + j + 1)] * y[j]
+        y_n += beta_inv(order, -(sz + j + 1)) * y[j]
     return y_n
 
 def adam_bashforth(y: list, t0: float, h: float, steps: int, f, order: int):
@@ -138,6 +152,7 @@ def formula_inversa(y: list, t0: float, h: float, steps: int, f, order: int):
 def main():
     freopen('input.txt', 'r')
     freopen('output.txt', 'w')
+    plot_points = []
     for line in sys.stdin:
         line = line.split()
         method = line[0].split('_')
@@ -198,9 +213,18 @@ def main():
                 points = runge_kutta([y0], t0, h, steps, f)
         print('y(', t0, ') =', points[0])
         print('h =', h)
+        t_s = []
+        for i in range(0, len(points)):
+            points[i] = float(points[i])
+            t_s.append(t0 + i * h)
         for i in range(0, len(points)):
             print(i, points[i])
         print('')
+        plot_points.append([t_s, points])
+    """ [!!!] for i in range(0, len(plot_points)):
+        plt.plot(plot_points[i][0], plot_points[i][1], 'ro')
+        plt.axis([0, plot_points[i][0][-1], 0, plot_points[i][1][-1]])
+        plt.show() [!!!] """
     return 0
 
 if __name__ == '__main__':
